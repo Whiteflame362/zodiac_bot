cab863689b7b6580e158471c76a4c81ddb877d82
Whiteflame362 <whiteflame362@gmail.com>
2022-08-01 23:36:01 -0400

    Made changes to zodiac_bot.py

--- bot/zodiac_bot.py
+++ bot/zodiac_bot.py
@@ -9,4 +9,16 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print('Bot is online')
 
+@bot.command()
+async def hello(ctx):
+    if (str(ctx.author) == "walt#9586"):
+        return
+    await ctx.channel.send(f'Hello {ctx.author.mention}!')
+
+@bot.command()
+async def goodbye(ctx):
+    if (str(ctx.author) == "walt#9586"):
+        return
+    await ctx.channel.send(f'Goodbye {ctx.author.mention}!')
+
 bot.run(os.getenv('TOKEN'))
